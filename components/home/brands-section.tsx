b3e66eb16d2cf37e0dd75cccc1b45b0114afb05d
Maksim Kulishevich <maxim8992012@gmail.com>
2025-06-30 13:54:37 +0300

    fix: change brands on home page

--- components/home/brands-section.tsx
+++ components/home/brands-section.tsx
@@ -18,33 +18,32 @@ function BrandsSection({ brands }: { brands: Brand[] }) {
           centerInsufficientSlides
           autoplay={{ delay: 2000 }}
           breakpoints={{
-            320: {
-              slidesPerView: 2,
-              spaceBetween: 32,
-            },
-            640: {
-              slidesPerView: 3,
-              spaceBetween: 32,
-            },
             1024: {
               slidesPerView: 4,
-              spaceBetween: 32,
+              spaceBetween: 140,
             },
           }}
+          spaceBetween={32}
+          slidesPerView={"auto"}
           modules={[Navigation, Autoplay]}
         >
           {brands.map((brand, idx) => {
             const { image_path, slug } = brand;
 
             return (
-              <SwiperSlide className="max-w-[200px]" key={idx}>
-                <Link href={`/catalog/brands/${slug}`}>
+              <SwiperSlide
+                className="sm:w-[200px] sm:h-[200px]  w-[120px] h-[120px]"
+                key={idx}
+              >
+                <Link
+                  href={`/catalog/brands/${slug}`}
+                  className="relative w-full h-full block"
+                >
                   <Image
-                    className="mx-auto object-contain h-[120px] sm:h-[200px]"
+                    className="object-contain"
                     src={`${process.env.NEXT_PUBLIC_STORAGE_URL}${image_path}`}
                     alt={slug}
-                    width={200}
-                    height={200}
+                    fill
                   />
                 </Link>
               </SwiperSlide>
